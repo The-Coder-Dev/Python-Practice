6e3c156a7dad4feec7b8c1ff79cb4cea69208eb1
Dev Sharma <devsharma2057@gmail.com>
2026-09-18 23:36:55 +0530

    Madlib Game

---
+++ madlib_game.py
@@ -0,0 +1,14 @@
+# MadLibs Game
+# A word game where you create a story
+# by filling in the blanks with random words
+
+adjective1 = input("Enter an adjective(description): ")
+adjective2 = input("Enter an adjective(description): ")
+noun1 = input("Enter a noun(person, place or thing): ")
+verb1 = input("Enter a verb ending with 'ing': ")
+verb2 = input("Enter a verb ending with 'ing': ")
+
+
+print(f"Today I walked through a {adjective1} forest.")
+print(f"On a branch, I spotted a {adjective2} {noun1}.")
+print(f"{noun1} suddenly {verb2}.")
